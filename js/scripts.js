91183ac850f978b3bcaa8d3a391854c52c4a0113
Andromeda Yelton <andromeda.yelton@gmail.com>
2015-10-25 18:27:16 -0400

    Hopefully more accessible

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
+	var overview = $('.overview');
+	var maintoggle = $('.toggle-main')
     $('#nav').onePageNav();
 
     $('a[href^="http"]').attr('target','_blank');
-    
+
     $('.toggle').click(function(){
-        $('.overview').toggleClass('open');
+        overview.toggleClass('open');
+        var state = maintoggle.attr('aria-expanded');
+        console.log(state);
+        if ( state=='true' ) {
+        	maintoggle.attr('aria-expanded', 'false');
+        } else {
+        	maintoggle.attr('aria-expanded', 'true');      	
+        }
     });
 });
